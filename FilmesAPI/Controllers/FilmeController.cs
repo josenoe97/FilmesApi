8e48e66ed06776831b697f1dcbbec994e2af24ba
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-08-27 10:58:34 -0300

    Aplicação dos dados direto no banco de dados

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers;
@@ -7,32 +8,43 @@ namespace FilmesAPI.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
+    private FilmeContext _context;
 
-    private static List<Filme> filmes = new List<Filme>();
-    private static int id = 0;
+    public FilmeController(FilmeContext context) // Aplicação direta no BD 
+    {
+        _context = context;
+    }
+
 
     [HttpPost]
     public IActionResult AdicionaFilme([FromBody] Filme filme) // Ação de Inserção de recurso no sistema
     {
-        filme.Id = id++;
-        filmes.Add(filme);
+        /*filme.Id = id++;
+        filmes.Add(filme);*/
+        _context.Filmes.Add(filme);
+        _context.SaveChanges(); // Salva alterações 
+
         // retorna o caminho que foi cadastrado
         return CreatedAtAction(nameof(RecuperaFilmePorId),
             new {id = filme.Id } , 
             filme);
+
     }
 
     [HttpGet]
     public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50) // Ação de Leitura
     {
-        return filmes.Skip(skip).Take(take);
+        /*return filmes.Skip(skip).Take(take);*/
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
     public  IActionResult RecuperaFilmePorId(int id)
     {
-        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
-        if(filme == null) return NotFound(); // returna caso a pagina não exista
+        /*var filme = filmes.FirstOrDefault(filme => filme.Id == id);*/
+        var filme = _context.Filmes
+            .FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound(); // returna caso a pagina não exista
         return Ok(filme);
         
     }
