35fda099862e4029671e183a5a1053b4f6634e17
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-08-31 19:15:30 -0300

    Criando model Cinema, Dtos de Cinema(Create,Update e Read), CinemaProfile para o AutoMapper,Controller CinemaController

---
+++ FilmesAPI/Profiles/CinemaProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Profiles
+{
+    public class CinemaProfile : Profile
+    {
+        public CinemaProfile()
+        {
+            CreateMap<CreateCinemaDto, Cinema>();
+            CreateMap<Cinema, CreateCinemaDto>();
+            CreateMap<UpdateCinemaDto, Cinema>();
+        }
+    }
+}
