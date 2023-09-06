76867aabcb16334df2d768a30610f2822da0f232
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-09-05 21:53:52 -0300

    Criacao de Sessao em Model,Dto,Controller, aplicacao no Contex e criacao de um relacionamento 1:n com Filmes

---
+++ FilmesAPI/Profiles/SessaoProfile.cs
@@ -0,0 +1,15 @@
+﻿using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using AutoMapper;
+
+namespace FilmesAPI.Profiles 
+{
+    public class SessaoProfile : Profile
+    {
+        public SessaoProfile()
+        {
+            CreateMap<CreateSessaoDto, Sessao>();
+            CreateMap<Sessao, ReadSessaoDto>();
+        }        
+    }
+}
